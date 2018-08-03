5211ea96736206c8e2bc6dff2b3c2e690639d3e1
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-08-03 14:55:51 -0400

    WIP Ingest eBird data into Postgres database

--- src/lib/base_04_ingest_ebird.py
+++ src/lib/base_04_ingest_ebird.py
@@ -1,7 +1,6 @@
 """Ingest eBird data."""
 
 from datetime import date
-import warnings
 import pandas as pd
 import lib.sqlite as db
 import lib.data as data
@@ -14,14 +13,18 @@ class BaseIngestEbird:
     EBIRD_PATH = db.DATA_DIR / 'raw' / DATASET_ID
     EBIRD_CSV = EBIRD_PATH / 'ebd_relFeb-2018.txt'
 
+    PLACE_KEY_COLUMNS = ['lng', 'lat']
     PLACE_COLUMNS = db.PLACE_COLUMNS + [
         'COUNTRY CODE', 'STATE CODE', 'COUNTY CODE', 'IBA CODE', 'BCR CODE',
         'USFWS CODE', 'ATLAS BLOCK', 'LOCALITY ID', ' LOCALITY TYPE',
         'EFFORT AREA HA']
-    EVENT_COLUMNS = db.EVENT_COLUMNS + [
+
+    EVENT_KEY_COLUMN = 'SAMPLING EVENT IDENTIFIER'
+    EVENT_COLUMNS = db.EVENT_COLUMNS + PLACE_KEY_COLUMNS + [
         'EFFORT AREA HA', 'APPROVED', 'REVIEWED', 'NUMBER OBSERVERS',
         'ALL SPECIES REPORTED', 'OBSERVATION DATE', 'GROUP IDENTIFIER']
-    COUNT_COLUMNS = db.COUNT_COLUMNS + [
+
+    COUNT_COLUMNS = db.COUNT_COLUMNS + EVENT_KEY_COLUMN + [
         'GLOBAL UNIQUE IDENTIFIER', 'LAST EDITED DATE', 'TAXONOMIC ORDER',
         'CATEGORY', 'SUBSPECIES SCIENTIFIC NAME', 'BREEDING BIRD ATLAS CODE',
         'BREEDING BIRD ATLAS CATEGORY', 'AGE/SEX', 'OBSERVER ID', 'HAS MEDIA']
@@ -99,12 +102,13 @@ class BaseIngestEbird:
 
     def _insert_places(self, raw_data, to_place_id):
         print(f'Inserting {self.DATASET_ID} places')
+        places = raw_data[self.PLACE_COLUMNS]
 
-        raw_data['key'] = tuple(zip(raw_data.lng, raw_data.lat))
-        dups = raw_data.key.duplicated()
-        places = raw_data[~dups]
+        places['place_key'] = self._get_place_keys(places)
+        dups = places.place_key.duplicated()
+        places = places[~dups]
 
-        old_places = places.key.isin(to_place_id)
+        old_places = places.place_key.isin(to_place_id)
         places = places[~old_places]
 
         is_na = places.radius.isna()
@@ -116,45 +120,68 @@ class BaseIngestEbird:
         places['dataset_id'] = self.DATASET_ID
         places = self.cxn.add_place_id(places)
 
-        new_place_ids = places.set_index('key').place_id.to_dict()
+        new_place_ids = places.set_index('place_key').place_id.to_dict()
         to_place_id = {**to_place_id, **new_place_ids}
+
+        places = places.drop(['place_key'], axis=1)
+        self.cxn.insert_places(places)
+
         return to_place_id
 
     def _insert_events(self, raw_data, to_place_id, to_event_id):
         print(f'Inserting {self.DATASET_ID} events')
-#         dups = df['SAMPLING EVENT IDENTIFIER'].duplicated()
-#         events = df[~dups]
-#         old_events = events['SAMPLING EVENT IDENTIFIER'].isin(sample_ids)
+        events = raw_data[self.EVENT_COLUMNS]
 
-#         df['year'] = df.event_date.dt.strftime('%Y')
-#         df['day'] = df.event_date.dt.strftime('%j')
+        events['place_key'] = self._get_place_keys(events)
 
-#         df['started'] = pd.to_datetime(df['started'], format='%H:%M:%S')
-#         df['delta'] = pd.to_numeric(df['DURATION MINUTES'], errors='coerce')
-#         df.delta = pd.to_timedelta(df.delta, unit='m', errors='coerce')
-#         df['ended'] = df.started + df.delta
-#         convert_to_time(df, 'started')
-#         convert_to_time(df, 'ended')
+        dups = events[self.EVENT_KEY_COLUMN].duplicated()
+        events = events[~dups]
 
+        old_events = events[self.EVENT_KEY_COLUMN].isin(to_event_id)
+        events = events[~old_events]
 
-#         events = events[~old_events]
+        events['place_id'] = events.place_key.map(to_place_id)
+        events['year'] = events.event_date.dt.strftime('%Y')
+        events['day'] = events.event_date.dt.strftime('%j')
+
+        events['started'] = pd.to_datetime(
+            events['started'], format='%H:%M:%S')
+        events['delta'] = pd.to_numeric(
+            events['DURATION MINUTES'], errors='coerce')
+        events.delta = pd.to_timedelta(events.delta, unit='m', errors='coerce')
+        events['ended'] = events.started + events.delta
+        self._convert_to_time(events, 'started')
+        self._convert_to_time(events, 'ended')
+
+        new_event_ids = events.set_index(
+            self.EVENT_KEY_COLUMN).event_id.to_dict()
+        to_event_id = {**to_event_id, **new_event_ids}
+
+        events = events.drop(['place_key'] + self.PLACE_KEY_COLUMNS, axis=1)
+        self.cxn.insert_events(events)
+
+        return to_event_id
+
     def _insert_counts(self, raw_data, to_event_id, to_taxon_id):
         print(f'Inserting {self.DATASET_ID} counts')
+        counts = raw_data[self.COUNT_COLUMNS]
 
-#         df['count'] = df['count'].apply(int)
-#         df['taxon_id'] = df['SCIENTIFIC NAME'].map(taxons)
+        counts['taxon_id'] = counts['SCIENTIFIC NAME'].map(to_taxon_id)
+        counts['event_id'] = counts[self.EVENT_KEY_COLUMN].map(to_event_id)
 
+        counts.count = counts.count.apply(int)
+        counts.event_id = counts.event_id.astype(int)
+        counts.taxon_id = counts.taxon_id.astype(int)
 
+        counts = self.cxn.add_count_id(counts)
+        counts = counts.drop([self.EVENT_KEY_COLUMN], axis=1)
+        self.cxn.insert_counts(counts)
 
-#         event_id = db.next_id(cxn, 'events')
-#         events['event_id'] = range(event_id, event_id + events.shape[0])
-
-#         events = events.set_index('SAMPLING EVENT IDENTIFIER', drop=False)
-#         new_sample_ids = events.event_id.to_dict()
-#         sample_ids = {**sample_ids, **new_sample_ids}
+    def _get_place_keys(self, df):
+        return tuple(zip(df.lng, df.lat))
 
     @staticmethod
-    def convert_to_time(df, column):
+    def _convert_to_time(df, column):
         """Convert the time field from datetime format to HH:MM format."""
         is_na = df[column].isna()
         df[column] = df[column].dt.strftime('%H:%M')
