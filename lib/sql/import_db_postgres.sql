f6f2401763699942cf003d87fbdc403c3c5d10de
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-10-04 11:34:07 -0400

    Change to use \copy in the sqlite to postgres data transfer script

--- lib/sql/import_db_postgres.sql
+++ lib/sql/import_db_postgres.sql
@@ -1,31 +1,20 @@
-\! date
-COPY version FROM '/museum/rafe/sightings/data/interim/version.csv' WITH (FORMAT csv);
+\copy version FROM '/museum/rafe/sightings/data/interim/version.csv' WITH (FORMAT csv);
 
-\! date
-COPY datasets FROM '/museum/rafe/sightings/data/interim/datasets.csv' WITH (FORMAT csv);
+\copy datasets FROM '/museum/rafe/sightings/data/interim/datasets.csv' WITH (FORMAT csv);
 
-\! date
-COPY countries FROM '/museum/rafe/sightings/data/interim/countries.csv' WITH (FORMAT csv);
+\copy countries FROM '/museum/rafe/sightings/data/interim/countries.csv' WITH (FORMAT csv);
 
-\! date
-COPY codes FROM '/museum/rafe/sightings/data/interim/codes.csv' WITH (FORMAT csv);
+\copy codes FROM '/museum/rafe/sightings/data/interim/codes.csv' WITH (FORMAT csv);
 
-\! date
-COPY taxons FROM '/museum/rafe/sightings/data/interim/taxons.csv' WITH (FORMAT csv);
+\copy taxons FROM '/museum/rafe/sightings/data/interim/taxons.csv' WITH (FORMAT csv);
 
-\! date
-COPY places FROM '/museum/rafe/sightings/data/interim/places.csv' WITH (FORMAT csv);
+\copy places FROM '/museum/rafe/sightings/data/interim/places.csv' WITH (FORMAT csv);
 
-\! date
-COPY events FROM '/museum/rafe/sightings/data/interim/events.csv' WITH (FORMAT csv);
+\copy events FROM '/museum/rafe/sightings/data/interim/events.csv' WITH (FORMAT csv);
 
-\! date
-COPY counts FROM '/museum/rafe/sightings/data/interim/counts.csv' WITH (FORMAT csv);
+\copy counts FROM '/museum/rafe/sightings/data/interim/counts.csv' WITH (FORMAT csv);
 
-\! date
 UPDATE places
    SET geopoint = ST_SetSRID(ST_MakePoint(lng, lat), 4326),
        geohash  = ST_GeoHash(ST_MakePoint(lng, lat), 7);
 COMMIT;
-
-\! date
