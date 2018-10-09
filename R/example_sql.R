87a684ca15b08eb7e5373491b077926206289e5d
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-10-09 16:53:16 -0400

    Updating DB with roles

--- R/example_sql.R
+++ R/example_sql.R
@@ -17,47 +17,13 @@ taxons <- tbl(cxn, "taxons")
 # This helps with efficiency. So, you may want to avoid the as.data.frame() function,
 # it is there only to show how to convert the queries into a dataframe.
 
-combined <- places %>%
-  inner_join(events, by = "place_id") %>%
-  inner_join(counts, by = "event_id") %>%
-  inner_join(taxons, by = "taxon_id") %>%
-  filter(between(year, 2010, 2014)) %>%
-  filter(between(day, 100, 200)) %>%
+all_tables <- places %>%
   filter(between(lng, -73, -72)) %>%
   filter(between(lat, 40, 44)) %>%
-  head(100) %>%
-  as.data.frame()
-
-
-bbs <- places %>%
   inner_join(events, by = "place_id") %>%
-  inner_join(counts, by = "event_id") %>%
-  inner_join(taxons, by = "taxon_id") %>%
   filter(between(year, 2010, 2014)) %>%
   filter(between(day, 100, 200)) %>%
-  filter(between(lng, -73, -72)) %>%
-  filter(between(lat, 40, 44)) %>%
-  head(100) %>%
-  as.data.frame()
-
-maps <- places %>%
-  inner_join(events, by = "place_id") %>%
   inner_join(counts, by = "event_id") %>%
   inner_join(taxons, by = "taxon_id") %>%
-  filter(between(year, 2010, 2014)) %>%
-  filter(between(day, 100, 200)) %>%
-  filter(between(lng, -73, -72)) %>%
-  filter(between(lat, 40, 44)) %>%
-  head(100) %>%
-  as.data.frame()
-
-ebird <- places %>%
-  inner_join(events, by = "place_id") %>%
-  inner_join(counts, by = "event_id") %>%
-  inner_join(taxons, by = "taxon_id") %>%
-  filter(between(year, 2010, 2014)) %>%
-  filter(between(day, 100, 200)) %>%
-  filter(between(lng, -73, -72)) %>%
-  filter(between(lat, 40, 44)) %>%
   head(100) %>%
   as.data.frame()
--- lib/sql/samples.sql
+++ lib/sql/samples.sql
@@ -9,3 +9,17 @@ SELECT *
    AND lat  BETWEEN   40 AND   44
    AND target = 't'
  LIMIT 100;
+
+
+ SELECT *
+   FROM places
+   JOIN events USING (place_id)
+   JOIN counts USING (event_id)
+   JOIN taxons USING (taxon_id)
+  WHERE year BETWEEN 2010 AND 2014
+    AND day  BETWEEN  100 AND  200
+    AND lng  BETWEEN  -73 AND  -72
+    AND lat  BETWEEN   40 AND   44
+    AND class  = 'lepidoptera'
+    AND target = 't'
+  LIMIT 100;
