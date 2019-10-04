231f59c4de00650151bf44a7c04053ab1217ce59
rafe <raphael.lafrance@gmail.com>
2019-10-04 08:31:29 -0400

    Get metadata about nabpp data

--- lib/nabpp_ingest.py
+++ lib/nabpp_ingest.py
@@ -23,28 +23,48 @@ SPLITTER = "','"    # Split single quoted fields
 #   quotechar tells us which type of file: sinlge or double quoted text
 #   cols = column count. This is used to check if the parsing
 CSVS = [
-    # {'file': 'test.txt', 'quotechar': SQ, 'cols': 37},
-    {'file': 'NABPP_2.txt', 'quotechar': SQ, 'cols': 6},
-    {'file': 'NABPP_adm1.txt', 'quotechar': DQ, 'cols': 5},
-    {'file': 'NABPP_cagns.txt', 'quotechar': DQ, 'cols': 9},
-    {'file': 'NABPP_discrepancy1.txt', 'quotechar': SQ, 'cols': 18},
-    {'file': 'NABPP_downloads.txt', 'quotechar': SQ, 'cols': 5},
-    {'file': 'NABPP_gnis.txt', 'quotechar': SQ, 'cols': 19},
-    {'file': 'NABPP_location1.txt', 'quotechar': SQ, 'cols': 7},
-    {'file': 'NABPP_obs_alias.txt', 'quotechar': SQ, 'cols': 2},
-    {'file': 'NABPP_observations.txt', 'quotechar': SQ, 'cols': 37},
-    {'file': 'NABPP_observer.txt', 'quotechar': SQ, 'cols': 12},
-    {'file': 'NABPP_rectifiable.txt', 'quotechar': SQ, 'cols': 3},
-    {'file': 'NABPP_rect_import.txt', 'quotechar': SQ, 'cols': 26},
-    {'file': 'NABPP_rect_listtest.txt', 'quotechar': SQ, 'cols': 1},
-    {'file': 'NABPP_rect_test.txt', 'quotechar': SQ, 'cols': 5},
-    {'file': 'NABPP_species_matched.txt', 'quotechar': DQ, 'cols': 9},
-    {'file': 'NABPP_species.txt', 'quotechar': DQ, 'cols': 4},
-    {'file': 'NABPP_transcriber.txt', 'quotechar': SQ, 'cols': 20},
-    {'file': 'NABPP_transcription.txt', 'quotechar': SQ, 'cols': 26},
-    {'file': 'NABPP_validatable.txt', 'quotechar': SQ, 'cols': 3},
-    {'file': 'NABPP_validated_obs_vw.txt', 'quotechar': DQ, 'cols': 3},
+    # {'file': 'test', 'quotechar': SQ, 'cols': 37},
+    {'file': 'NABPP_2', 'quotechar': SQ, 'cols': 6},
+    {'file': 'NABPP_adm1', 'quotechar': DQ, 'cols': 5},
+    {'file': 'NABPP_cagns', 'quotechar': DQ, 'cols': 9},
+    {'file': 'NABPP_discrepancy1', 'quotechar': SQ, 'cols': 18},
+    {'file': 'NABPP_downloads', 'quotechar': SQ, 'cols': 5},
+    {'file': 'NABPP_gnis', 'quotechar': SQ, 'cols': 19},
+    {'file': 'NABPP_location1', 'quotechar': SQ, 'cols': 7},
+    {'file': 'NABPP_obs_alias', 'quotechar': SQ, 'cols': 2},
+    {'file': 'NABPP_observations', 'quotechar': SQ, 'cols': 37},
+    {'file': 'NABPP_observer', 'quotechar': SQ, 'cols': 12},
+    {'file': 'NABPP_rectifiable', 'quotechar': SQ, 'cols': 3},
+    {'file': 'NABPP_rect_import', 'quotechar': SQ, 'cols': 26},
+    {'file': 'NABPP_rect_listtest', 'quotechar': SQ, 'cols': 1},
+    {'file': 'NABPP_rect_test', 'quotechar': SQ, 'cols': 5},
+    {'file': 'NABPP_species_matched', 'quotechar': DQ, 'cols': 9},
+    {'file': 'NABPP_species', 'quotechar': DQ, 'cols': 4},
+    {'file': 'NABPP_transcriber', 'quotechar': SQ, 'cols': 20},
+    {'file': 'NABPP_transcription', 'quotechar': SQ, 'cols': 26},
+    {'file': 'NABPP_validatable', 'quotechar': SQ, 'cols': 3},
+    {'file': 'NABPP_validated_obs_vw', 'quotechar': DQ, 'cols': 3},
     ]
+
+
+def reformat_csvs():
+    """Reformatting the CSVs."""
+    for table in CSVS:
+        print('Reformatting', table['file'])
+        _, path = get_file_names(table)
+        df = pd.read_csv(
+            path,
+            quotechar=table['quotechar'],
+            dtype=object,       # Silences but doesn't fix warnings
+            encoding='latin-1')
+        df.to_csv(path, index=False)
+
+
+def get_file_names(table):
+    """Get the source and destination file name."""
+    src = SRC_DIR / (table['file'] + '.txt')
+    dst = DST_DIR / (table['file'] + '.csv')
+    return src, dst
 
 
 def fix_files():
@@ -52,9 +72,7 @@ def fix_files():
     for table in CSVS:
         print('Fixing', table['file'])
 
-        src = SRC_DIR / table['file']
-        dst = DST_DIR / (table['file'][:-3] + 'csv')
-        table['file'] = dst
+        src, dst = get_file_names(table)
 
         if table['quotechar'] == SQ:
             fix_single_quotes(table, src, dst)
@@ -72,8 +90,9 @@ def check_files():
 
 def check_with_pandas(table):
     """Pandas finds more errors."""
+    _, path = get_file_names(table)
     pd.read_csv(
-        table['file'],
+        path,
         quotechar=table['quotechar'],
         dtype=object,       # Silences but doesn't fix warnings
         encoding='latin-1')
@@ -81,7 +100,8 @@ def check_with_pandas(table):
 
 def check_with_csv(table):
     """CSV gives more information about where errors are in the file."""
-    with open(table['file'], encoding='latin-1') as in_file:
+    _, path = get_file_names(table)
+    with open(path, encoding='latin-1') as in_file:
         reader = csv.DictReader(in_file, quotechar=table['quotechar'])
         for row in reader:
             if len(row) != table['cols']:
@@ -135,6 +155,31 @@ def fix_single_quotes(table, src, dst):
             out_file.write(line)
 
 
+def get_metadata():
+    """Build raw metadata for the tables."""
+    rows = []
+    for table in CSVS:
+        print('Metadata', table['file'])
+        _, path = get_file_names(table)
+        with open(path, encoding='latin-1') as in_file:
+            reader = csv.reader(in_file)
+            header = next(reader)
+        rows += [{'table': table['file'],
+                  'column': '',
+                  'primary_key': '',
+                  'foreign_key1': '',
+                  'foreign_key2': '',
+                  'foreign_key3': ''}]
+        cols = [{'table': table['file'], 'column': c} for c in header]
+        rows += cols
+
+    df = pd.DataFrame(rows)
+
+    df.to_csv(DST_DIR / 'metadata.csv', index=False)
+
+
 if __name__ == '__main__':
-    fix_files()
-    check_files()
+    # fix_files()
+    # check_files()
+    # reformat_csvs()
+    get_metadata()
